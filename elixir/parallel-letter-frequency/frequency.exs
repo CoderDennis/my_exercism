5669cdf680994fc081a6788aa0834eff1b36ee12
CoderDennis <dodava@gmail.com>
2016-03-25 11:03:58 -0500

    all tests pass, but ignoring workers param and just doing one task per text

--- elixir/parallel-letter-frequency/frequency.exs
+++ elixir/parallel-letter-frequency/frequency.exs
@@ -9,12 +9,21 @@ defmodule Frequency do
   @spec frequency([String.t], pos_integer) :: Map
   def frequency([], _), do: %{}
   def frequency([text], _), do: frequency_for_text(text)
-  def frequency(texts, workers) do
-
+  def frequency(texts, _workers) do
+    texts
+    |> Enum.map(fn t -> Task.async(__MODULE__, :frequency_for_text, [t]) end)
+    |> Task.yield_many()
+    |> Enum.map(fn {_task, {:ok, result}} -> result end)
+    |> Enum.reduce(fn(x, acc) ->
+      Map.merge(acc, x, fn _k, v1, v2 ->
+        v1 + v2
+      end)
+    end)
   end
 
-  defp frequency_for_text(text) do
+  def frequency_for_text(text) do
     text
+    |> String.strip
     |> String.downcase
     |> String.codepoints
     |> frequency_of_letters(%{})
@@ -22,14 +31,16 @@ defmodule Frequency do
 
   defp frequency_of_letters([], acc), do: acc
   defp frequency_of_letters([h | t], acc) do
-    frequency_of_letters(t, Map.update(acc, h, 1, &(&1 + 1)))
+    new_acc = case is_letter(h) do
+                true -> Map.update(acc, h, 1, &(&1 + 1))
+                false -> acc
+    end
+    frequency_of_letters(t, new_acc)
+  end
+
+  defp is_letter(l) do
+    String.downcase(l) != String.upcase(l)
   end
 
 end
 
-defmodule Frequency.Worker do
-  use GenServer
-
-  
-
-end
