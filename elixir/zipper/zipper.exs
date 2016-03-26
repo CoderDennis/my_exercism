53ec785adec87f6cea9f368524656e1f7ad60283
CoderDennis <dodava@gmail.com>
2016-03-26 04:23:02 -0500

    starting over with only trail

--- elixir/zipper/zipper.exs
+++ elixir/zipper/zipper.exs
@@ -9,89 +9,70 @@ defmodule BinTree do
   """
   @type t :: %BinTree{ value: any, left: BinTree.t | nil, right: BinTree.t | nil }
   defstruct value: nil, left: nil, right: nil
-
-  @spec parent(BT.t, BT.t) :: BT.t
-  def parent(root, root), do: nil
-  def parent(%BT{left: left} = bt, left), do: bt
-  def parent(%BT{right: right} = bt, right), do: bt
-  def parent(%BT{left: left, right: right}, leaf) do
-    parent(left, leaf) || parent(right, leaf)
-  end
-
-  @spec replace_node(BT.t, BT.t, BT.t) :: BT.t
-  def replace_node(nil, _, _), do: nil
-  def replace_node(bt, bt, new), do: new
-  def replace_node(bt, old, new) do
-    %{bt |
-      left: replace_node(bt.left, old, new),
-      right: replace_node(bt.right, old, new)}
-  end
 end
 
 defmodule Zipper do
   alias __MODULE__, as: Z
   alias BinTree, as: BT
-  @type t :: %Zipper{ tree: BT.t, focus: BT.t }
-  defstruct tree: nil, focus: nil
+  @moduledoc """
+  Can this be implemented with only a trail that acts like a stack?
+
+  The head of the trail is the focused node.
+  """
+  @type t :: %Zipper{ trail: [BT.t] }
+  defstruct trail: []
 
   @doc """
   Get a zipper focused on the root node.
   """
   @spec from_tree(BT.t) :: Z.t
-  def from_tree(bt), do: %Z{tree: bt, focus: bt}
+  def from_tree(bt), do: %Z{trail: [bt]}
 
   @doc """
   Get the complete tree from a zipper.
   """
   @spec to_tree(Z.t) :: BT.t
-  def to_tree(%Z{tree: bt}), do: bt
+  def to_tree(%Z{trail: t}), do: List.last(t)
 
   @doc """
   Get the value of the focus node.
   """
   @spec value(Z.t) :: any
-  def value(%Z{focus: f}), do: f.value
+  def value(%Z{trail: [f|_]}), do: f.value
 
   @doc """
   Get the left child of the focus node, if any.
   """
   @spec left(Z.t) :: Z.t | nil
-  def left(%Z{focus: f} = z), do: focus_result(z, f.left)
+  def left(z), do: nil
 
   @doc """
   Get the right child of the focus node, if any.
   """
   @spec right(Z.t) :: Z.t | nil
-  def right(%Z{focus: f} = z), do: focus_result(z, f.right)
+  def right(z), do: nil
 
   @doc """
   Get the parent of the focus node, if any.
   """
   @spec up(Z.t) :: Z.t | nil
-  def up(%Z{tree: bt, focus: f} = z), do: focus_result(z, BT.parent(bt, f))
-
-  defp focus_result(_, nil), do: nil
-  defp focus_result(z, f), do: %{z | focus: f}
+  def up(z), do: nil
 
   @doc """
   Set the value of the focus node.
   """
   @spec set_value(Z.t, any) :: Z.t
-  def set_value(%Z{focus: f} = z, v), do: replace_focus(z, %{f | value: v})
+  def set_value(z, v), do: nil
 
   @doc """
   Replace the left child tree of the focus node.
   """
   @spec set_left(Z.t, BT.t) :: Z.t
-  def set_left(%Z{focus: f} = z, l), do: replace_focus(z, %{f | left: l})
+  def set_left(z, l), do: nil
 
   @doc """
   Replace the right child tree of the focus node.
   """
   @spec set_right(Z.t, BT.t) :: Z.t
-  def set_right(%Z{focus: f} = z, r), do: replace_focus(z, %{f | right: r})
-
-  defp replace_focus(%Z{tree: bt, focus: f}, new_f) do
-    %Z{tree: BT.replace_node(bt, f, new_f), focus: new_f}
-  end
+  def set_right(z, r), do: nil
 end
