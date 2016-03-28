94c4c5fed95c5e249c60f8e90025660b0c14e31e
CoderDennis <dodava@gmail.com>
2016-03-28 01:40:17 -0500

    refactored dot-dsl

--- elixir/dot-dsl/dot.exs
+++ elixir/dot-dsl/dot.exs
@@ -3,39 +3,38 @@ defmodule Graph do
 end
 
 defmodule Dot do
-  defmacro graph(do: block) do
-    # IO.inspect(block)
-    quote bind_quoted: [
-      block: Macro.escape(block, unquote: false)
-    ] do
-      Dot.parse(block)
-    end
+  alias Graph, as: G
+
+  defmacro graph(do: ast) do
+    # IO.inspect(ast)
+    Dot.parse(ast) |> Macro.escape
   end
 
-  def parse(nil), do: %Graph{}
-  def parse({:graph, _, [attrs]}), do: %Graph{attrs: attrs}
-  def parse({:--, _, [{a, _, _}, {b, _, nil}]}) do
-    %Graph{edges: [{a, b, []}]}
-  end
-  def parse({:--, _, [{a, _, _}, {b, _, [opts]}]}) do
-    %Graph{edges: [{a, b, opts}]}
-  end
+  def parse(nil), do: %G{}
+  def parse({:graph, _, [attrs]}), do: %G{attrs: attrs}
+  def parse({:--, _, [{a, _, _}, {b, _, nil}]}), do: %G{edges: [{a, b, []}]}
+  def parse({:--, _, [{a, _, _}, {b, _, [opts]}]}), do: %G{edges: [{a, b, opts}]}
   def parse({:__block__, _, items}) do
     items
     |> Enum.map(&parse/1)
-    |> Enum.reduce(fn g, acc ->
+    |> merge_graphs
+  end
+  def parse({name, _, nil}), do: %G{nodes: [{name, []}]}
+  def parse({name, _, [opts]}) do
+    unless Keyword.keyword?(opts) do
+      raise ArgumentError
+    end
+    %G{nodes: [{name, opts}]}
+  end
+  def parse(_), do: raise ArgumentError
+
+  @spec merge_graphs([Graph.t]) :: Graph.t
+  defp merge_graphs(graphs) do
+    Enum.reduce(graphs, fn g, acc ->
       Map.merge(g, acc, fn
         :__struct__, v1, _v2 -> v1
         _k, v1, v2 -> Enum.concat(v1, v2) |> Enum.sort()
       end)
     end)
   end
-  def parse({name, _, nil}), do: %Graph{nodes: [{name, []}]}
-  def parse({name, _, [opts]}) do
-    unless Keyword.keyword?(opts) do
-      raise ArgumentError
-    end
-    %Graph{nodes: [{name, opts}]}
-  end
-  def parse(_), do: raise ArgumentError
 end
