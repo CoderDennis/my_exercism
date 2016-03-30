a94d97aceaac210daa001630307b7295afa66fb7
CoderDennis <dodava@gmail.com>
2016-03-29 20:07:09 -0500

    bob solution

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -1,5 +1,18 @@
 module Bob where
 
+import String exposing (..)
+
 hey : String -> String
 hey input =
-  "Whatever."
+  if String.trim input == "" then
+    "Fine. Be that way!"
+
+  else if String.toUpper input == input &&
+          String.toLower input /= input then
+    "Whoa, chill out!"
+
+  else if String.endsWith "?" input then
+    "Sure."
+
+  else
+    "Whatever."
