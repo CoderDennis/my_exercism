201d243561a2d6d014b0ae4df49f5c2e844d1673
CoderDennis <dodava@gmail.com>
2016-03-31 23:08:32 -0500

    refactored Bob

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -4,15 +4,27 @@ import String exposing (..)
 
 hey : String -> String
 hey input =
-  if String.trim input == "" then
+  if saysNothing input then
     "Fine. Be that way!"
 
-  else if String.toUpper input == input &&
-          String.toLower input /= input then
+  else if isShouting input then
     "Whoa, chill out!"
 
-  else if String.endsWith "?" input then
+  else if asksQuestion input then
     "Sure."
 
   else
     "Whatever."
+
+saysNothing : String -> Bool
+saysNothing input =
+  String.trim input == ""
+
+isShouting : String -> Bool
+isShouting input =
+  String.toUpper input == input &&
+  String.toLower input /= input
+
+asksQuestion : String -> Bool
+asksQuestion input =
+  String.endsWith "?" input
