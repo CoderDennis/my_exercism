e2fb20d3a64bce3f545f60fd4c9ec0493745c535
CoderDennis <dodava@gmail.com>
2016-04-01 17:57:15 -0500

    solved accumulate

--- elm/accumulate/Accumulate.elm
+++ elm/accumulate/Accumulate.elm
@@ -1,1 +1,9 @@
 module Accumulate (..) where
+
+accumulate : (a -> a) -> List a -> List a
+accumulate fun list =
+  case list of
+    [] -> []
+
+    first :: rest ->
+      fun first :: accumulate fun rest
