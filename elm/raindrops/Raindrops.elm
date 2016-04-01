a29cc809fc743727082c4bfdecd78bf41101fa03
CoderDennis <dodava@gmail.com>
2016-03-31 23:07:33 -0500

    raindrops solution

--- elm/raindrops/Raindrops.elm
+++ elm/raindrops/Raindrops.elm
@@ -1,6 +1,26 @@
-module Raindrops (..) where
+module Raindrops (raindrops) where
 
 raindrops : Int -> String
 raindrops num =
-  toString num
+  case pling num ++ plang num ++ plong num of
+    "" -> (toString num)
+    x  -> x
 
+pling : Int -> String
+pling num =
+  drop num 3 "Pling"
+
+plang : Int -> String
+plang num =
+  drop num 5 "Plang"
+
+plong : Int -> String
+plong num =
+  drop num 7 "Plong"
+
+drop : Int -> Int -> String -> String
+drop num factor string =
+  if num % factor == 0 then
+    string
+  else
+    ""
