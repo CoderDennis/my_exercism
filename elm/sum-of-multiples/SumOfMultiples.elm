9a1f50b5160cbbfa472fdeb36296d5609bbfffe9
CoderDennis <dodava@gmail.com>
2016-04-20 21:04:31 -0500

    extracted divides function

--- elm/sum-of-multiples/SumOfMultiples.elm
+++ elm/sum-of-multiples/SumOfMultiples.elm
@@ -6,8 +6,13 @@ sumOfMultiples : List Int -> Int -> Int
 sumOfMultiples multiples num =
   let
     isMultiple n =
-      List.any (\m -> n % m == 0) multiples
+      multiples
+        |> List.any (divides n)
   in
     [1..num-1]
       |> List.filter isMultiple
       |> List.sum
+
+divides : Int -> Int -> Bool
+divides dividend divisor =
+  dividend % divisor == 0
