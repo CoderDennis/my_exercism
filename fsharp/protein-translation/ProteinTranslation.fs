a05464107e1616e1cb93c3a59d1dfd814a2ccdac
CoderDennis <dodava@gmail.com>
2016-09-27 23:54:57 -0500

    more F#

---
+++ fsharp/protein-translation/ProteinTranslation.fs
@@ -0,0 +1,26 @@
+﻿module ProteinTranslation
+
+open System
+
+let private toCodons (rna: string) =
+    rna
+    |> Seq.chunkBySize 3
+    |> Seq.map String.Concat
+
+let private toProtein =
+    function
+    | "AUG"                         -> "Methionine"
+    | "UUU" | "UUC"                 -> "Phenylalanine"
+    | "UUA" | "UUG"                 -> "Leucine"
+    | "UCU" | "UCC" | "UCA" | "UCG" -> "Serine"
+    | "UAU" | "UAC"                 -> "Tyrosine"
+    | "UGU" | "UGC"                 -> "Cysteine"
+    | "UGG"                         -> "Tryptophan"
+    | "UAA" | "UAG" | "UGA"         -> "STOP"
+    | _ -> failwith "unknown"
+
+let translate rna =
+    rna
+    |> toCodons
+    |> Seq.map toProtein
+    |> Seq.takeWhile ((<>) "STOP")
---
+++ fsharp/proverb/Proverb.fs
@@ -0,0 +1,14 @@
+﻿module Proverb
+
+let private items = ["nail"; "shoe"; "horse"; "rider"; "message"; "battle"; "kingdom"]
+
+let line =
+    function
+    | x when x < 7 && x > -1 -> sprintf "For want of a %s the %s was lost." items.[x-1] items.[x]
+    | 7 -> "And all for the want of a horseshoe nail."
+    | _ -> "Invalid line number"
+
+let proverb = 
+    [1..7]
+    |> Seq.map line
+    |> String.concat "\n"
